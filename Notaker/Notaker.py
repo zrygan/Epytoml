183851f19aaab3b5b166500f1641aa3f0b4e451b
Z1aaan <zheanrobbyganituen@gmail.com>
2021-11-24 21:15:16 +0800

    Commit

--- Notaker/Notaker.py
+++ Notaker/Notaker.py
@@ -1,2 +1,17 @@
-def shortcut(NAME):
-    print(NAME)
+# Epytomil, Notaker
+# Created by: Zhean Ganituen
+
+# TODO:
+
+
+def ntkGen(fileName, directory):
+    # NTK File is the output of the Notaker, append all user inputs here.
+    finalFileName = fileName + ".html"
+    
+    global ntkFile
+    ntkFile = open(finalFileName, "w")
+    ntkFile.write("<!DOCTYPE html>\n")
+
+
+def h1(text):
+    pass
