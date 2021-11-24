266d06039768673a89aee03f5e12645576bde791
Z1aaan <zheanrobbyganituen@gmail.com>
2021-11-24 22:54:44 +0800

    Inital Commit

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 from Notaker import Notaker as ntk
+from Bake import ntkBake as bk
 
-ntk.ntkGen("test", "test")
+ntk.ntkGen("Test 1")
+ntk.h1("Chapter 1")
+ntk.h1("Chapter 2")
+ntk.toc(2)
 ntk.ntkShut()
+
+bk()
